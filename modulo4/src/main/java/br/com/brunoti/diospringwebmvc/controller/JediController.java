f3fa2e23b06489073f25a3ee6396fe72421663c7
Bruno Rocha <bruno.roch@gmail.com>
2021-10-23 17:59:38 -0300

    module4 - add GetMapping /new-jedi

--- modulo4/src/main/java/br/com/brunoti/diospringwebmvc/controller/JediController.java
+++ modulo4/src/main/java/br/com/brunoti/diospringwebmvc/controller/JediController.java
@@ -21,4 +21,15 @@ public class JediController {
 
 		return modelAndView;
 	}
+
+	@GetMapping("/new-jedi")
+	public ModelAndView createJedi() {
+
+		final ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("new-jedi");
+
+		modelAndView.addObject("jedi", new Jedi());
+
+		return modelAndView;
+	}
 }
